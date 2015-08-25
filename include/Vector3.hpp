abb8f059864adb70dd83e5b687512d97f01edbd9
Tyler Scott <tyduptyler13@gmail.com>
2015-08-24 21:55:36 -0600

    Added to Vector3

--- include/Vector3.hpp
+++ include/Vector3.hpp
@@ -1,7 +1,13 @@
 #ifndef MYUPLAY_MYENGINE_VECTOR3
 #define MYUPLAY_MYENGINE_VECTOR3
 
+#include <cmath>
 
+#include "Matrix3.hpp"
+#include "Matrix4.hpp"
+#include "Quanternion.hpp"
+#include "Euler.hpp"
+#include "Camera.hpp"
 
 namespace MyUPlay {
 
@@ -252,18 +258,118 @@ namespace MyUPlay {
 			Vector3& roundToZero();
 			
 			Vector3& negate(){
+				x = -x;
+				y = -y;
+				z = -x;
 
+				return *this;
 			}
 
 			inline Vector3 operator-(){
-				
+				negate();
 			}
 			
-			Vector3& dot(const Vector3& v){
+			T dot(const Vector3& v){
+				return x * v.x + y * v.y + z * v.z;
+			}
+
+			T lengthSq(){
+				return x * x + y * y + z * z;
+			}
+
+			T length(){
+				return std::sqrt(lengthSq());
+			}
+
+			T lenghtManhattan(){
+				return std::abs(x) + std::abs(y) + std::abs(z);
+			}
+
+			T normalize(){
+				divide(length());
+			}
+
+			Vector3& setLength(T l){
+				T oldLength = length();
+
+				if (oldLength != 0 && l != oldLength){
+					multiply(l / oldLength);
+				}
+
+				return this;
+			}
+
+			Vector3& lerp(const Vector3& v, T alpha){
+				x += (v.x - x) * alpha;
+				y += (v.y - y) * alpha;
+				z += (v.z - z) * alpha;
+
+				return *this;
+			}
+
+			Vector3& lerpVectors(const Vector3& v1, const Vector3& v2, T alpha){
+				subVectors(v2, v1).multiply(alpha).add(v1);
+
+				return *this;
+			}
+
+			Vector3& cross(const Vector3& v){
+				T x = this->x, y = this->y, z = this->z;
+				this->x = y * v.z - z * v.y;
+				this->y = z * v.x - x * v.z;
+				this->z = x * v.y - y * v.x;
+
+				return *this;
+			}
+
+			Vector3& cross(const Vector3& a, const Vector3& b){
+
+				x = a.y * b.z - a.z * b.y;
+				y = a.z * b.x - a.x * b.z;
+				z = a.x * b.y - a.y * b.x;
+
+				return *this;
 
 			}
 
+			Vector3& projectOnVector(const Vector3&);
+			Vector3& projectOnPlane(const Vector3& normal);
+			Vector3& reflect(const Vector3& normal);
+			T angleTo(const Vector3&);
+			
+			T distanceTo(const Vector3& v){
+				return std::sqrt(distanceToSquared(v));
+			}
+
+			T distanceToSquared(const Vector3& v){
+				T dx = x - v.x;
+				T dy = y - v.y;
+				T dz = z - v.z;
+
+				return dx * dx + dy * dy + dz * dz;
+			}
+
+			Vector3& setFromMatrixPosition(const Matrix4&);
+			Vector3& setFromMatrixScale(const Matrix4&);
+			Vector3& setFromMatrixColumn(unsigned index, const Matrix4&);
+			
+			bool operator==(const Vector3& v){
+				return ((x == v.x) && (y == v.y) && (z == v.z));
+			}
+
+			//TODO
+
 		};
+		
+		template<typename T>
+		Vector3<T> operator+(const T scalar, Vector3<T> rhs){
+			return rhs + scalar;
+		}
+
+		template<typename T>
+		Vector3<T> operator*(const T scalar, Vector3<T> rhs){
+			return rhs * scalar;
+		}
 
 		typedef Vector3<> Vector3f;
 		typedef Vector3<double> Vector3d;
