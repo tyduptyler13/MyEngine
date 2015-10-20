06cc078bbde2f11ab3fbebf5c8b5f4292afaad75
Ethan <Preevete@gmail.com>
2015-10-20 14:40:34 -0600

    finished Vector2.hpp and Vector2.cpp

--- include/Vector2.hpp
+++ include/Vector2.hpp
@@ -1,6 +1,7 @@
 #ifndef MYUPLAY_MYENGINE_VECTOR2
 #define MYUPLAY_MYENGINE_VECTOR2
 
+#include <cmath>
 
 
 namespace MyUPlay {
@@ -115,8 +116,8 @@ namespace MyUPlay {
 
             Vector2& floor(){
 
-                x = floor(x);
-                y = floor(y);
+                x = std::floor(x);
+                y = std::floor(y);
 
                 return *this;
 
@@ -124,8 +125,8 @@ namespace MyUPlay {
 
             Vector2& ceil(){
 
-                x = ceil(x);
-                y = ceil(y);
+                x = std::ceil(x);
+                y = std::ceil(y);
 
                 return *this;
 
@@ -133,8 +134,8 @@ namespace MyUPlay {
 
             Vector2& round(){
 
-                x = round(x);
-                y = round(y);
+                x = std::round(x);
+                y = std::round(y);
 
                 return *this;
 
@@ -142,12 +143,107 @@ namespace MyUPlay {
 
             Vector2& roundToZero() {
 
-                x = x < 0 ? ceil(x) : floor(x);
-                y = y < 0 ? ceil(y) : floor(y);
+                x = x < 0 ? std::ceil(x) : std::floor(x);
+                y = y < 0 ? std::ceil(y) : std::floor(y);
 
                 return *this;
 
             }
+
+            inline Vector2 operator-() const {
+                return Vector2(*this).negate();
+            }
+
+            bool operator==(const Vector2& v) const {
+                return ((x ==v.x) && (y == v.y));
+            }
+
+            Vector2 operator+(const T scalar, Vector2 rhs){
+                return rhs + scalar;
+            }
+
+            Vector2 operator*(const T scalar, Vector2 rhs){
+                return rhs * scalar;
+            }
+
+            inline Vector2& operator=(const Vector2& v){
+                return copy(v);
+            }
+
+            inline Vector2& operator+=(const Vector2& v){
+                return add(v);
+            }
+
+            inline Vector2& operator +=(T scalar){
+                return add(scalar)
+            }
+
+
+            Vector2 operator+(const Vector2& b) const{
+                Vector2 v;
+                v.x = x + b.x;
+                v.y = y + b.y;
+                return v;
+            }
+
+            inline Vector2 operator+(T s) const{
+                return Vector2(x + s, y + s);
+            }
+
+            inline Vector2& operator-=(const Vector2& v){
+                return sub(v);
+            }
+
+            inline Vector2& operator-=(T scalar){
+                return sub(scalar);
+            }
+
+            inline Vector2 operator*=(const Vector2& v){
+                return multiply(v);
+            }
+
+            Vector2 operator*(T s) const{
+                Vector2 v;
+                v.x = x * s;
+                v.y = y * s;
+                return v;
+            }
+
+            Vector2 operator*(const Vector2& b) const{
+                Vector2 v;
+                v.x = x * b.x;
+                v.y = y * b.y;
+                return v;
+            }
+
+            Vector2& divide(T scalar){
+                x /= scalar;
+                y /= scalar;
+                return *this;
+            }
+
+            inline Vector2& operator/=(const Vector2& v){
+                return divide(v);
+            }
+
+            inline Vector2& operator/=(T scalar){
+                return divide(scalar);
+            }
+
+            Vector2 operator/(const Vector2& v) const{
+                Vector2 b;
+                b.x = x / v.x;
+                b.y = y / v.y;
+                return b;
+            }
+
+            Vector2 operator/(T s) const{
+                Vector2 v;
+                v.x = x / s;
+                v.y = y / s;
+                return v;
+            }
+
 
 			Vector2& min(const Vector2&);
 			Vector2& max(const Vector2&);
