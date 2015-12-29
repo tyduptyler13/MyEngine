ba51ecf9294ddf28110008dfa8f4c04a7f93cdc6
Tyler Scott <tyduptyler13@gmail.com>
2015-12-28 18:42:58 -0700

    Work on the renderer

--- include/GLES2Renderer.hpp
+++ include/GLES2Renderer.hpp
@@ -10,6 +10,10 @@
 
 #include "Renderer.hpp"
 #include "Log.hpp"
+#include "Frustrum.hpp"
+#include "Camera.hpp"
+#include "Vector3.hpp"
+#include "Matrix4.hpp"
 
 namespace MyUPlay {
 
@@ -23,6 +27,45 @@ namespace MyUPlay {
 			SDL_Window* window;
 			SDL_GLContext* gl;
 			Log& log = Log::getInstance();
+
+			template <typename T>
+			static bool checkExtension(std::string name){
+				if (T){
+					return true;
+				} else {
+					log << name + " is not available.";
+					return false;
+				}
+			}
+
+			GLuint mCurrentProgram,
+			       mCurrentFramebuffer;
+			int mCurrentMaterialId = -1;
+			//TODO mCurrentGeometryProgram
+			int mCurrentCamera -1;
+
+			unsigned mUsedTextureUnits = 0;
+
+			unsigned mViewportX = 0,
+				 mViewportY = 0,
+				 mViewportWidth,
+				 mViewportHeight,
+				 mCurrentWidth = 0,
+				 mCurrentHeight = 0;
+
+			Frustum<float> mFrustum;
+
+			Matrix4f mProjScreenMatrix;
+
+			Vector3f mVector3;
+
+			Vector3f direction;
+
+			bool lightsNeedUpdate = true;
+
+			struct {
+				//TODO
+			} mLights;
 
 			void setDefaultGLState(){
 				//state?
@@ -51,6 +94,8 @@ namespace MyUPlay {
 					log << "Failed to load opengl!";
 					throw new std::runtime_error("Could not get opengl handle.");
 				}
+
+				checkExtension<GLEW_OES_texture_float>("OES_texture_float");
 
 				//TODO capabilities, state, properties, objects, programcache?
 				//
